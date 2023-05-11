93622e80eba6dcd7ead2b68e899512333bca1670
hv-dev <harryv@chillipharm.com>
2023-05-11 15:13:46 +0100

    chore: index files for components

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+/*
+Export components built within this directory from this file,
+so it can be picked up by this repos top level index.js
+*/
+import Header from "./Header"
+
+export {
+  Header
+};
---
+++ src/components/Note/index.js
@@ -0,0 +1,9 @@
+/*
+Export components built within this directory from this file,
+so it can be picked up by this repos top level index.js
+*/
+import Note from "./Note"
+
+export {
+  Note
+};
