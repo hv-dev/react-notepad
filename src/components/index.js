1c3b1c11c10c4a7ba3810726504f559db6c7766e
hv-dev <harryv@chillipharm.com>
2023-05-15 16:48:41 +0100

    chore: moved Header into presentational folder

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@
 Export components built within this directory from this file,
 so it can be picked up by this repos top level index.js
 */
-import Header from "./Header"
+import Header from "./presentational/Header"
 // import Footer from "./Footer"
 // import NoteForm from "./NoteForm"
 // import Note from "./Note"
