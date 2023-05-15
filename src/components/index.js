62c20bedf67aaf4089ccc4db46621e376d059c59
hv-dev <harryv@chillipharm.com>
2023-05-15 17:23:36 +0100

    chore: moving components into presentational or container folders

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,9 @@ Export components built within this directory from this file,
 so it can be picked up by this repos top level index.js
 */
 import Header from "./presentational/Header"
-// import Footer from "./Footer"
-// import NoteForm from "./NoteForm"
-// import Note from "./Note"
+// import Footer from "./presentational/Footer"
+// import NoteForm from "./container/NoteForm"
+// import Note from "./presentational/Note"
 
 export {
   Header,
