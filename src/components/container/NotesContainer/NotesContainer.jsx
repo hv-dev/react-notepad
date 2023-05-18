d5c78e5028fe17ac96dc7cfa3bf1043e27e9bf0f
hv-dev <harryv@chillipharm.com>
2023-05-18 14:26:55 +0100

    feat: moved existing Note related components into NotesContainer component

---
+++ src/components/container/NotesContainer/NotesContainer.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import styled from "styled-components"
+import { NoteForm } from "../../"
+
+function NotesContainer() {
+
+
+    return (
+        <main>
+            <NoteForm />
+            
+        </main>
+    )
+}
+
+export default NotesContainer
--- src/components/index.js
+++ src/components/index.js
@@ -3,13 +3,14 @@ Export components built within this directory from this file,
 so it can be picked up by this repos top level index.js
 */
 import Header from "./presentational/Header"
+import NotesContainer from "./container/NotesContainer"
 import NoteForm from "./container/NoteForm"
 // import Footer from "./presentational/Footer"
 // import Note from "./presentational/Note"
 
 export {
   Header,
-  // Footer,
+  NotesContainer,
   NoteForm
-  // Note
+  // Footer,
 };
